defef629e6d95ead49e8430aa90791d71a3e737d
Victor-B-Reyes <victor_blanco_reyes@outlook.com>
2025-01-09 22:44:43 -0600

    inputs

--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -3,7 +3,13 @@ import React from "react";
 function Login(){
     return(
         <div>
-            <form action="" method="post"></form>
+            <form action="" method="post">
+                <h1>Nombre</h1>
+                <input type="text" name="" id="" />
+                <h1>Contraseña</h1>
+                <input type="password" name="" id="" />
+
+            </form>
         </div>
     )
 }
